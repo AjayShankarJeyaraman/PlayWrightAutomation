2c895c3bdc043a8e5b0bcfd0de06cd59c2cbe19d
AjayShankarJeyaraman <ajayshankar918@gmail.com>
2025-07-09 09:00:06 +0530

    Clear playwright

--- PlayWrightAutomation/src/test/java/Automation/BrowserLaunch.java
+++ PlayWrightAutomation/src/test/java/Automation/BrowserLaunch.java
@@ -12,6 +12,9 @@ public  static void main(String [] args ) {
 	
 	
 	
+	
+	
+	
 	Playwright playwright = Playwright.create();
 	
 	Browser browser =playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setChannel("chrome"));
